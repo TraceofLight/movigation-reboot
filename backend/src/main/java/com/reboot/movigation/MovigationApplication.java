443cdbb34e509c7ebad4e530d0c5664af2d1452a
TraceofLight <98262849+TraceofLight@users.noreply.github.com>
2023-04-12 00:54:54 +0900

    Feat: Init SpringBoot
    Java SpringBoot 시작

---
+++ backend/src/main/java/com/reboot/movigation/MovigationApplication.java
@@ -0,0 +1,13 @@
+package com.reboot.movigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovigationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovigationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/reboot/movigation/MovigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reboot.movigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovigationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
