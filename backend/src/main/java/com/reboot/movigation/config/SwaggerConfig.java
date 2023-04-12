b70f1ef965e6445913114524c61ebe1e387faf2b
TraceofLight <rickyjun45@gmail.com>
2023-04-12 15:27:06 +0900

    Feat: Add Swagger Page
    스웨거 페이지 추가

--- backend/src/main/java/com/reboot/movigation/config/SwaggerConfig.java
+++ backend/src/main/java/com/reboot/movigation/config/SwaggerConfig.java
@@ -1,40 +1,34 @@
 package com.reboot.movigation.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
-import java.util.function.Predicate;
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Movigation RESTful API",
+                description = "영화 추천 플랫폼 Movigation의 명세서",
+                version = "1.0.0"
+        )
+)
 
 @Configuration
-@EnableSwagger2
+@RequiredArgsConstructor
 public class SwaggerConfig {
-
     // url: movigation.app
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Movigation Spring Boot REST API")
-                .version("1.0.0")
-                .description("영화 추천 플랫폼 Movigation의 Descriptions")
-                .build();
-    }
-
-    public Docket getDocket(String groupName, Predicate<String> predicate) {
-        return new Docket(DocumentationType.SWAGGER_2).groupName(groupName).apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.reboot.movigation")).paths(predicate::test)
-                .build();
-    }
-
     @Bean
-    public Docket userApi() {
-        return getDocket("회원", (path) -> path.matches("api/v1/movigation/user.*"));
-    }
+    public GroupedOpenApi UserApi() {
+        String[] paths = {"/api/v1/movigation/user/**"};
 
+        return GroupedOpenApi
+                .builder()
+                .group("User API")
+                .pathsToMatch(paths)
+                .build();
+    }
 
 }
